54c916713a1cf64ec0c1ce973affa284f344abd4
Lucas Ceruti <lucas.ceeruti@gmail.com>
2022-11-22 16:16:45 -0300

    Incializando o projeto

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css'
+
+import Title from "../title/Title"
+import SubTitle from "../subTitle/SubTitle"
+
+
+function Header () {
+    return (
+        <div className={styles.header}>
+            <Title>Jogo da Velha</Title>
+            <SubTitle>Criado por Lucas Ceruti</SubTitle>
+        </div>
+    )
+}
+
+export default Header
