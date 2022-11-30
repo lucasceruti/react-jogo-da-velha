e3c601f7db821411b2573dc7413a10085ac7667d
Lucas Ceruti <lucas.ceeruti@gmail.com>
2022-11-30 18:21:35 -0300

    Criado logica para declarar campeão e Reset

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,72 +1,70 @@
-import { useState, useEffect } from 'react'
-import styles from './Game.module.css'
-import Icon from '../icon/Icon'
-import GameOption from '../gameOption/GameOption'
+import { useState, useEffect } from "react";
+import styles from "./Game.module.css";
+import GameOption from "../gameOption/GameOption";
+import GameInfo from "../gameInfo/GameInfo";
 
 const winnerTable = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-function Game () {
-    const [gameState, setGameState] = useState(Array(9).fill(0))
-    const [currentPlayer, setCurrentPlayer] = useState(-1)
-    const [winner, setWinner] = useState(0)
+function Game() {
+  const [gameState, setGameState] = useState(Array(9).fill(0));
+  const [currentPlayer, setCurrentPlayer] = useState(-1);
+  const [winner, setWinner] = useState(0);
 
-    const handleClick = (pos) => {
-        if (gameState[pos] === 0 && winner === 0) {
-            let newGameState = [...gameState]
-            newGameState[pos] = currentPlayer
-            setGameState(newGameState)      
-        }
+  const handleClick = (pos) => {
+    if (gameState[pos] === 0 && winner === 0) {
+      let newGameState = [...gameState];
+      newGameState[pos] = currentPlayer;
+      setGameState(newGameState);
     }
-    const verifyGame = () => {
-        winnerTable.forEach((line) => {
-            const values = line.map((pos) => gameState[pos])
-            const sum = values.reduce((sum, value) => sum + value)
-            if (sum === 3 || sum === -3) 
-            setWinner(sum /3)
-        })
-    }
+  };
+  const verifyGame = () => {
+    winnerTable.forEach((line) => {
+      const values = line.map((pos) => gameState[pos]);
+      const sum = values.reduce((sum, value) => sum + value);
+      if (sum === 3 || sum === -3) {
+        setWinner(sum / 3);
+      }
+    });
+  };
 
-    /*useEffect parametros = arrow function e array*/
-    useEffect( () => {
-        setCurrentPlayer(currentPlayer * -1)
-        verifyGame()
-    }, [gameState]) 
+  const handleReset= () => {
+    setGameState(Array(9).fill(0))
+    setWinner(0)
+  }
 
-    return (
-        <div className={styles.gameContent}>
-            <div className={styles.game}>
-                {
-                    gameState.map(( value, pos) => 
-                    <GameOption 
-                        key={`game-option-pos-${pos}`}
-                        status={value}
-                        onclick={() => handleClick(pos)}
-                        /> 
-                    )
-                }
+  /*useEffect parametros = arrow function e array*/
+  useEffect(() => {
+    setCurrentPlayer(currentPlayer * -1);
+    verifyGame();
+  }, [gameState]);
 
-            </div>
-
-            <div className={styles.gameInfo}>
-                <h4>Próximo a jogar :</h4>
-                {
-                    currentPlayer === 1 && <Icon iconName="circle" />
-                }
-                {
-                    currentPlayer === -1 && <Icon iconName="x" />
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.gameContent}>
+      <div className={styles.game}>
+        {gameState.map((value, pos) => (
+          <GameOption
+            key={`game-option-pos-${pos}`}
+            status={value}
+            onclick={() => handleClick(pos)}
+          />
+        ))}
+      </div>
+      <GameInfo 
+        currentPlayer={currentPlayer}
+        winner={winner}
+        onReset={handleReset}
+      />
+    </div>
+  );
 }
 
-export default Game
+export default Game;
