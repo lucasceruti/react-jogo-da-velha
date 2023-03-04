5b3751a099b41152ea9b4157fe3d7c4fa14390e8
Lucas Ceruti <lucas.ceeruti@gmail.com>
2023-03-04 14:49:05 -0300

    Criado componente Icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,14 +2,21 @@ import styles from './Header.module.css'
 
 import Title from "../title/Title"
 import SubTitle from "../subTitle/SubTitle"
+import Icon from '../icon/Icon'
 
 function Header () {
     return (
         <div className={styles.header}>
             <Title>Jogo da Velha</Title>
             <SubTitle>Criado por Lucas Ceruti</SubTitle>
+            <div className={styles.iconContent}>
+                <Icon 
+                    iconName="github"
+                    link="https://github.com/lucasceruti"/>
+            </div>
         </div>
     )
 }
+
 
 export default Header
