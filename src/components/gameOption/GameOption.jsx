2365ce468582a6c5ba4737472440facf6012e8ab
Lucas Ceruti <lucas.ceeruti@gmail.com>
2023-03-04 21:52:24 -0300

    Criado estados no gameOption

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,8 +1,19 @@
 import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption () {
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25pc"/>
+
+function GameOption ({ status }) {
     return (
-        <div className={styles.gameOption}></div>
+        <div className={styles.gameOption}>
+            {
+                status == 1 && <GameIcon iconName = "circle" size="25px"/>
+            }
+            {
+                status == -1 && <GameIcon iconName = "x" size="25px"/>
+            }
+
+        </div>
     )
 }
 
