e53c10bf153368dd6ad83ca0bd06e75214133bbc
Lucas Ceruti <lucas.ceeruti@gmail.com>
2023-03-04 22:14:04 -0300

    Criado lógica do Game

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -3,9 +3,9 @@ import Icon from '../icon/Icon'
 
 const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25pc"/>
 
-function GameOption ({ status }) {
+function GameOption ({ status, onClick }) {
     return (
-        <div className={styles.gameOption}>
+        <div className={styles.gameOption} onClick ={onClick}>
             {
                 status == 1 && <GameIcon iconName = "circle" size="25px"/>
             }
