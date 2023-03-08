7d7142d2e70665454ca9bb3d1542235a44797e83
Lucas Ceruti <lucas.ceeruti@gmail.com>
2023-03-08 13:27:12 -0300

    Criado condicional de vencedor CSS

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,74 +1,78 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import styles from './Game.module.css'
-import GameOption from '../gameOption/GameOption'
-import GameInfo from '../gameInfo/GameInfo'
+import styles from "./Game.module.css";
+import GameOption from "../gameOption/GameOption";
+import GameInfo from "../gameInfo/GameInfo";
 
 const winnerTable = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
+function Game() {
+  const [gameState, setGameState] = useState(Array(9).fill(0));
+  const [currentPlayer, setCurrentPlayer] = useState(-1);
+  const [winner, setWinner] = useState(0);
+  const [winnerLine, setWinnerLine] = useState([]);
 
-function Game () {
-    const [gameState, setGameState] = useState(Array(9).fill(0))
-    const [currentPlayer, setCurrentPlayer] = useState(-1)
-    const [winner, setWinner] = useState(0)
-
-    const handleClick = (pos) => {
-        if (gameState[pos] === 0 && winner === 0 ) {
-            let newGameState = [...gameState]
-            newGameState[pos] = currentPlayer
-            setGameState(newGameState)
-        }
+  const handleClick = (pos) => {
+    if (gameState[pos] === 0 && winner === 0) {
+      let newGameState = [...gameState];
+      newGameState[pos] = currentPlayer;
+      setGameState(newGameState);
     }
+  };
 
-    const verifyGame = () => {
-        winnerTable.forEach((line) => {
-            const values = line.map((pos) => gameState[pos])
-            const sum = values.reduce((acc , value) => acc + value)
-            if(sum === 3 || sum === -3) {
-                setWinner (sum / 3)
-            }
-        })
-    }
+  const verifyGame = () => {
+    winnerTable.forEach((line) => {
+      const values = line.map((pos) => gameState[pos]);
+      const sum = values.reduce((acc, value) => acc + value);
+      if (sum === 3 || sum === -3) {
+        setWinner(sum / 3);
+        setWinnerLine(line);
+      }
+    });
+  };
 
-    const handleReset = () => {
-        setGameState(Array(9).fill(0))
-        setWinner(0)
-    }
+  const handleReset = () => {
+    setGameState(Array(9).fill(0));
+    setWinner(0);
+    setWinnerLine([]);
+  };
 
-    useEffect(() => {
-        setCurrentPlayer(currentPlayer * -1)
-        verifyGame()
-    }, [gameState])
+  const verifyWinnerLine = (pos) =>
+    winnerLine.find((value) => value === pos) !== undefined;
 
-    return (
-        <div className={styles.gameContent}>
-            <div className={styles.game}>
-            {
-                gameState.map(( value, pos) => 
-                <GameOption 
-                key={`game-option-pos-${pos}`}
-                status={value}
-                onClick={() => handleClick(pos)}
-                />
-                )
-            }
-        </div>
-        <GameInfo 
-            currentPlayer={currentPlayer}
-            winner={winner}
-            onReset={handleReset}
-        />
-        </div>
-    )
+  useEffect(() => {
+    setCurrentPlayer(currentPlayer * -1);
+    verifyGame();
+  }, [gameState]);
+
+  return (
+    <div className={styles.gameContent}>
+      <div className={styles.game}>
+        {gameState.map((value, pos) => (
+          <GameOption
+            key={`game-option-pos-${pos}`}
+            status={value}
+            onClick={() => handleClick(pos)}
+            isWinner={verifyWinnerLine(pos)}
+          />
+        ))}
+      </div>
+      <GameInfo
+        currentPlayer={currentPlayer}
+        winner={winner}
+        onReset={handleReset}
+      />
+    </div>
+  );
 }
 
-export default Game
+export default Game;
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,20 +1,23 @@
-import styles from './GameOption.module.css'
-import Icon from '../icon/Icon'
+import classNames from "classnames";
 
-const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25pc"/>
+import styles from "./GameOption.module.css";
+import Icon from "../icon/Icon";
 
-function GameOption ({ status, onClick }) {
-    return (
-        <div className={styles.gameOption} onClick ={onClick}>
-            {
-                status == 1 && <GameIcon iconName = "circle" size="25px"/>
-            }
-            {
-                status == -1 && <GameIcon iconName = "x" size="25px"/>
-            }
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25pc" />;
 
-        </div>
-    )
+function GameOption({ status, onClick, isWinner }) {
+  return (
+    <div
+      className={classNames(styles.gameOption, {
+        [styles.winner]: isWinner,
+      })
+    }
+      onClick={onClick}
+    >
+      {status == 1 && <GameIcon iconName="circle" size="25px" />}
+      {status == -1 && <GameIcon iconName="x" size="25px" />}
+    </div>
+  );
 }
 
-export default GameOption
+export default GameOption;
